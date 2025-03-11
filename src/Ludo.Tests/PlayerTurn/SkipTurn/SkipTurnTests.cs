165e921aada76d1ef890043b6bd021d11def7921
Esben Dam Cramer <edc@aguardio.com>
2025-03-11 13:27:43 +0100

    Unit Tests

---
+++ src/Ludo.Tests/PlayerTurn/SkipTurn/SkipTurnTests.cs
@@ -0,0 +1,17 @@
+namespace Ludo.Tests.PlayerTurn.SkipTurn;
+
+public class SkipTurnTests
+{
+    [Fact]
+    public void SkipTurn_NoValidMove_SkipTurn()
+    {
+        
+    }
+
+    [Fact]
+    public void SkipTurn_NoValidMoveButRolled6_ExtraTurn()
+    {
+        
+    }
+}
+   
