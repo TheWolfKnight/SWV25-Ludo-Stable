2505335fc58503c7d839de233378a7d76b6e9afa
Esben Dam Cramer <edc@aguardio.com>
2025-03-11 13:10:21 +0100

    Init push

---
+++ src/Ludo.Tests/PlayerTurn/RollDice/RollDiceTests.cs
@@ -0,0 +1,23 @@
+namespace Ludo.Tests.PlayerTurn.RollDice;
+
+public class RollDiceTests
+{
+    /// <summary>
+    /// Missing methods/states?
+    /// </summary>
+    [Fact]
+    public void RollDie_OnMyTurn_RollDie()
+    {
+        // Arrange
+        
+        // Act
+        
+        // Assert
+    }
+
+    [Fact]
+    public void RollDie_NotOnMyTurn_NotRollDie()
+    {
+        
+    }
+}
