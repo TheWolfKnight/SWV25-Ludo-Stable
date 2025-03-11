9d383ca4a128b14237586c860261f0c81b20b9f7
Esben Dam Cramer <edc@aguardio.com>
2025-03-11 14:04:46 +0100

    Added tiles to Board

--- src/Ludo.Common/Models/Board.cs
+++ src/Ludo.Common/Models/Board.cs
@@ -1,9 +1,11 @@
 using System;
+using Ludo.Common.Models.Tiles;
 
 namespace Ludo.Common.Models;
 
 public class Board
 {
+  public required TileBase[] Tiles { get; init; }
   public void LoadNewBoard()
   {
     throw new NotImplementedException();
