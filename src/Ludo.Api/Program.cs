96fdf925b2f5e97c5f970bf6197918f18e0ad5d2
Philip Knudsen <philipalexknudsen@gmail.com>
2025-04-22 09:04:34 +0200

    add swagger

--- src/Ludo.Api/Program.cs
+++ src/Ludo.Api/Program.cs
@@ -5,12 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
