18b44c7f4137e3d1229e49ffc6e53ec8b7a3a5e4
GameTweak <esbendam@live.dk>
2025-05-19 22:08:30 +0200

    Ydeligere implmentering af Setup

--- src/Ludo.Blazor/Pages/SetupPage.razor.cs
+++ src/Ludo.Blazor/Pages/SetupPage.razor.cs
@@ -8,23 +8,34 @@ namespace Ludo.Blazor.Pages;
 public partial class SetupPage : ComponentBase
 {
     [Inject]
-    public required GameService Service { get; set; }
+    public required GameService GameService { get; set; }
     
     [Inject]
-    public required GameState GameState { get; set; }
+    public required DieService DieService { get; set; }
     
+    private GameState? _gameState;
     private List<PlayerSetup> _players = [];
     
     private bool _isGameCreated = false;
 
     private async Task CreateGameAsync()
     {
-        GameState = await Service.GetNewGameAsync(_players.Count);
+        _gameState = await GameService.GetNewGameAsync(_players.Count);
+        
+        _players.ForEach(x => x.CanRoll = true);
+        _isGameCreated = true;
+        
+        StateHasChanged();
     }
 
     private async Task RollDiceAsync(int playerNr)
     {
-        //
+        int roll = await DieService.RollDieAsync(_gameState, _gameState.Die.PeekRoll());
+        
+        _players.First(x => x.PlayerNr == playerNr).Roll = roll;
+        _players.First(x => x.PlayerNr == playerNr).CanRoll = false;
+        
+        StateHasChanged();
     }
     
     private void AddPlayer()
