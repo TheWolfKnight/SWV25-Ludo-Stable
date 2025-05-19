3a02444119dee4db3456c486c3080c028893f665
Philip Alexander Lundgaard Knudsen <philipalexknudsen@gmail.com>
2025-05-19 23:21:59 +0200

    stuff

--- src/Ludo.Blazor/Features/Game/PlayerService.cs
+++ src/Ludo.Blazor/Features/Game/PlayerService.cs
@@ -10,37 +10,10 @@ namespace Ludo.Blazor.Features.Game;
 public class PlayerService
 {
   private readonly HttpClient _httpClient;
-  private readonly DieFactory _dieFactory;
 
   public PlayerService(HttpClient httpClient, DieFactory dieFactory)
   {
     _httpClient = httpClient;
     _dieFactory = dieFactory;
-  }
-
-  public async Task<GameState> MakeMoveAsync(GameState state, int pieceTilePosition, int roll)
-  {
-    const string url = "v1/move";
-
-    MakeMoveRequestDto request = new()
-    {
-      Game = state.ToDto(),
-      PiecePosition = pieceTilePosition,
-      Roll = roll
-    };
-
-    HttpResponseMessage response = await _httpClient.PostAsJsonAsync(url, request);
-
-    if (response.StatusCode is not HttpStatusCode.OK)
-    {
-      //TODO: this
-    }
-
-    GameDto? game = await response.Content.ReadFromJsonAsync<GameDto>();
-    if (game is null)
-      throw new InvalidOperationException("Could not deserialize response to GameDto object");
-
-    GameState result = GameState.FromDto(game, _dieFactory);
-    return result;
   }
 }
